70b1dfe5b41200b1e0663179a9037dbf6796cf06
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2021-11-10 21:37:09 +0100

    Hello World
    Solución de settings.py y urls.py + conexión entre urls.py y views.py para   devolver un Hello World

--- outfitShowroomApp/views.py
+++ outfitShowroomApp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.http import HttpResponse
 from .models import Estilo, Ocasion, Outfit, Prenda
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+	return HttpResponse("Hello, world!")
+
+#def nombreEmpresa(request, nombre_empresa):
+#	lista_empresas = get_list_or_404(Outfit, nombre=Outfit.nombre)
+    #context = {}
+#	return render(request, 'listaEmpresas.html', context)	#render busca internamente en carpeta llamada templates, si no tendríamos que añadir la ruta completa
+	#cambiar render por EmpresaDetailView
