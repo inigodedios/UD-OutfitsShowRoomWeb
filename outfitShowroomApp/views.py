c7f7729dd63e47ed53a96a0a1c91e098fad0430e
Iñigo <71695682+inigodedios@users.noreply.github.com>
2021-11-11 17:56:05 +0100

    Adding templates and some configuration

--- outfitShowroomApp/views.py
+++ outfitShowroomApp/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.http import HttpResponse
+from django.template import Template, Context, context 
 from .models import Estilo, Ocasion, Outfit, Prenda
 
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, world!")
+
+def prueba(request): #prueba
+	nombre = 'Prueba'
+	context = {
+		'login': True
+		'nombre': nombre #TODO
+	}	
+	return render(request, prueba.html, context)
+
+def prueba(request):
+	departamentos = get_list_or_404(Prenda.objects.order_by('nombre'))
+	context = {'lista_departamentos': departamentos }
+	return render(request, 'index.html', context)
+
+
 
 #def nombreEmpresa(request, nombre_empresa):
 #	lista_empresas = get_list_or_404(Outfit, nombre=Outfit.nombre)
