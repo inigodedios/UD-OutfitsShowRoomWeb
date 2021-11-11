fb39410a8f89a391326616a3d20a47a5f8462317
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2021-11-11 20:57:19 +0100

    Attributes correction
    Corrección en max_length de atributos para permitir crear objetos más completos.

--- outfitShowroomApp/models.py
+++ outfitShowroomApp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Prenda(models.Model):
     #Campo para la relación
     #models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     id_pr = models.AutoField(auto_created=True, primary_key=True)
-    disenador = models.CharField(max_length=40)
+    disenador = models.CharField(max_length=4000)
     precio = models.DecimalField(decimal_places=2, max_digits=5, default=0)
     
     TALLAS = (
@@ -16,15 +16,15 @@ class Prenda(models.Model):
     )
 
     talla = models.CharField(max_length=1, choices=TALLAS)
-    origen = models.CharField(max_length=40)
-    cuidados = models.CharField(max_length=40)
-    materiales = models.CharField(max_length=40)
+    origen = models.CharField(max_length=4000)
+    cuidados = models.CharField(max_length=4000)
+    materiales = models.CharField(max_length=4000)
 
     def __str__(self):
         return "{} ".format(self.nombre, self.disenador, self.id_pr, self.precio, self.talla, self.origen, self.cuidados, self.materiales)
 
 class Ocasion(models.Model):
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     desc = models.CharField(max_length=4000)
     id_oc = models.AutoField(auto_created=True, primary_key=True)
 
@@ -34,14 +34,14 @@ class Ocasion(models.Model):
 class Estilo(models.Model):
     id_est = models.AutoField(auto_created=True, primary_key=True)
     desc = models.CharField(max_length=4000)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
 
     def str(self):
         return self.nombre, self.desc, self.id_est
 
 class Outfit(models.Model):
     id_out = models.AutoField(auto_created=True, primary_key=True)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     desc = models.CharField(max_length=4000)
     precio = models.DecimalField(decimal_places=2, max_digits=5, default=0)
 
