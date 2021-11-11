fc8b714729a6450b3f834733f1231501f62cad5e
Iñigo <71695682+inigodedios@users.noreply.github.com>
2021-11-11 19:31:31 +0100

    Administration panel configurated
    Comprueba que ves los modelos desde /admin @aidagomezbueno

--- outfitShowroomApp/admin.py
+++ outfitShowroomApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from outfitShowroomApp.modelsg import Prenda, Outfit, Ocasion, Estilo
 # Register your models here.
+admin.site.register(Prenda)
+admin.site.register(Outfit)
+admin.site.register(Ocasion)
+admin.site.register(Estilo)
