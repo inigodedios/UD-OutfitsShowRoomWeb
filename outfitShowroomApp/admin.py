1a3c0e63018d5d3715a9a9eac600f77183b28314
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2021-11-11 23:11:04 +0100

    Changes admin.py & fixed some errors in models.py

--- outfitShowroomApp/admin.py
+++ outfitShowroomApp/admin.py
@@ -1,32 +1,34 @@
 from django.contrib import admin
-from outfitShowroomApp.models import Prenda, Outfit, Ocasion, Estilo
+from .models import Prenda, Outfit, Ocasion, Estilo
 
 
 class PrendaAdmin(admin.ModelAdmin):
     list_display=("id_pr", "nombre", "tipo_prenda", "talla", "precio", "materiales",)
-    search_fields=("id_pr", "nombre", "tipo_prenda","talla", "precio", "materiales")
-    list_filter=("talla", "tipo_prenda")
-# class OcasionAdmin(admin.ModelAdmin):
-#     list_display=("id_pr", "nombre", "materiales", "talla", "precio")
-#     search_fields=("id_pr", "nombre", "materiales", "talla", "precio")
-#     list_filter=("talla",)
+    search_fields=("id_pr", "nombre", "tipo_prenda","talla", "precio", "materiales",)
+    list_filter=("talla", "tipo_prenda",)
+    ordering = ("id_pr",)
 
-# class EstiloAdmin(admin.ModelAdmin):
-#     list_display=("id_pr", "nombre", "materiales", "talla", "precio")
-#     search_fields=("id_pr", "nombre", "materiales", "talla", "precio")
-#     list_filter=("talla",)
+class OcasionAdmin(admin.ModelAdmin):
+    list_display=("id_oc", "nombre", "desc",)
+    search_fields=("id_oc", "nombre", "desc",)
+    list_filter=("nombre",)
+    ordering = ("id_oc",)
 
-# class OutfitAdmin(admin.ModelAdmin):
-#     list_display=("id_pr", "nombre", "materiales", "talla", "precio")
-#     search_fields=("id_pr", "nombre", "materiales", "talla", "precio")
-#     list_filter=("talla",)
+class EstiloAdmin(admin.ModelAdmin):
+    list_display=("id_est", "nombre", "desc",)
+    search_fields=("id_est", "nombre", "desc",)
+    list_filter=("nombre",)
+    ordering = ("id_est",)
+
+class OutfitAdmin(admin.ModelAdmin):
+    list_display=("id_out", "nombre", "desc", "precio",) #TODO "estilo", "ocasiones", "prendas",)
+    search_fields=("id_out", "nombre", "desc", "precio",) #TODO "estilo", "ocasiones", "prendas",)
+    list_filter=("nombre",) #TODO "estilo", "ocasiones", "prendas",)
+    ordering = ("id_out",)
 
 
 # Register your models here.
 admin.site.register(Prenda, PrendaAdmin)
-# admin.site.register(Ocasion, OcasionAdmin)
-# admin.site.register(Estilo, EstiloAdmin)
-# admin.site.register(Outfit, OutfitAdmin)
-admin.site.register(Ocasion)
-admin.site.register(Estilo)
-admin.site.register(Outfit)
+admin.site.register(Ocasion, OcasionAdmin)
+admin.site.register(Estilo, EstiloAdmin)
+admin.site.register(Outfit, OutfitAdmin)
--- outfitShowroomApp/models.py
+++ outfitShowroomApp/models.py
@@ -13,54 +13,58 @@ class Prenda(models.Model):
     )
 
     TIPOS_PRENDA = (
-        ('null', 'null'), #La base de datos necesita rellenar las filas con algo
+        (' ', ' '), #La base de datos necesita rellenar las filas con algo
         ('Abrigo', 'Abrigo'),
-        ('Chaqueta', 'Chaqueta'),
-        ('Zapatos', 'Zapatos'),
-        ('Camisa', 'Camisa'),
-        ('Pantalón', 'Pantalón'),
-        ('Blazer', 'Blazer'),
-        ('Camisa', 'Camisa'),
-        ('Top', 'Top'),
-        ('Camiseta', 'Camiseta'),
-        ('Sudadera', 'Sudadera'),
         ('Accesorio', 'Accesorio'),
+        ('Blazer', 'Blazer'),
+        ('Body', 'Body'),
+        ('Botines', 'Botines'),
+        ('Camisa', 'Camisa'),
+        ('Camiseta', 'Camiseta'),
+        ('Cazadora', 'Cazadora'),
+        ('Chaqueta', 'Chaqueta'),
+        ('Jeans', 'Jeans'),
+        ('Pantalón', 'Pantalón'),
+        ('Plumífero', 'Plumífero'),
+        ('Sudadera', 'Sudadera'),
+        ('Top', 'Top'),
         ('Vestido', 'Vestido'),
+        ('Zapatos', 'Zapatos'),
     )
 
     id_pr = models.AutoField(auto_created=True, primary_key=True)
-    nombre = models.CharField(max_length=40)
-    disenador = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
+    disenador = models.CharField(max_length=4000)
     #date = models.DateField(_("Date"), default=datetime.date.today) #TODO especificar que solo queremos mes y año
     talla = models.CharField(max_length=1, choices=TALLAS)
     precio = models.DecimalField(decimal_places=2, max_digits=5, default=0)
-    tipo_prenda = models.CharField(max_length=100, choices=TIPOS_PRENDA, default="null") #TODO ¿Por qué es encesario poner un default en tipo y no en talla?
-    origen = models.CharField(max_length=40)
-    materiales = models.CharField(max_length=40)
-    cuidados = models.CharField(max_length=40)
+    tipo_prenda = models.CharField(max_length=100, choices=TIPOS_PRENDA, default=" ") #TODO ¿Por qué es encesario poner un default en tipo y no en talla?
+    origen = models.CharField(max_length=4000)
+    materiales = models.CharField(max_length=4000)
+    cuidados = models.CharField(max_length=4000)
     
     def __str__(self):
-        return "{} ".format(self.id_pr, self.nombre, self.disenador, self.talla, self.precio, self.tipo, self.origen, self.materiales, self.cuidados)
+        return "{}".format(self.id_pr, self.nombre, self.disenador, self.talla, self.precio, self.tipo_prenda, self.origen, self.materiales, self.cuidados)
 
 class Ocasion(models.Model):
     id_oc = models.AutoField(auto_created=True, primary_key=True)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     desc = models.CharField(max_length=4000)
     
-    def str(self):
-        return "{} " .format(self.id_oc, self.nombre,)
+    def __str__(self):
+        return "{} {}".format(self.id_oc, self.nombre, )
 
 class Estilo(models.Model):
     id_est = models.AutoField(auto_created=True, primary_key=True)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     desc = models.CharField(max_length=4000)
     
-    def str(self):
-        return "{} " .format(self.id_est, self.nombre,)
+    def __str__(self):
+        return "{} ".format(self.id_est, self.nombre, )
 
 class Outfit(models.Model):
     id_out = models.AutoField(auto_created=True, primary_key=True)
-    nombre = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=4000)
     desc = models.CharField(max_length=4000)
     precio = models.DecimalField(decimal_places=2, max_digits=5, default=0)
 
@@ -70,4 +74,4 @@ class Outfit(models.Model):
     prendas = models.ManyToManyField(Prenda, related_name='outfitsprendas')
 
     def __str__(self):
-        return "{} " .format(self.id_out, self.nombre, self.precio, )
+        return "{} ".format(self.id_out, self.nombre, self.precio, )
