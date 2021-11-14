1efce1f45ab3d7e535495443dda96c4bb04575cd
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2021-11-14 20:03:31 +0100

    Form Template
    Plantilla adaptada para la página del formulario contacto.html y enviado.html.

--- outfitShowroomApp/forms.py
+++ outfitShowroomApp/forms.py
@@ -6,8 +6,10 @@ class FormularioContacto(forms.Form):
     nombre = forms.CharField()
     apellido = forms.CharField()
     correo = forms.EmailField()
+    descripcion = forms.CharField()
 
     ACEPT=[('Acepto el tratamiento de mis datos.','Acepto el tratamiento de mis datos.'),]
 
     condiciones = forms.ChoiceField(choices=ACEPT, widget=forms.RadioSelect)
+    
 
